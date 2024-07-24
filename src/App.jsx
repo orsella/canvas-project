e26b177773f2ead108d3c7322dfbbf50ddf1c98d
skgtjt1 <justin.tosh.14@ucl.ac.uk>
2024-07-24 12:01:25 +0100

    basic structure implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 // import { useState } from 'react'
 import "./App.css";
+import Header from "./components/Header";
+import Canvas1 from "./components/Canvas1";
+import Canvas2 from "./components/Canvas2";
+import Canvas3 from "./components/Canvas3";
+import Canvas4 from "./components/Canvas4";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Canvas</h1>
-      </div>
-    </>
+    <body className="body">
+      <Header></Header>
+      <main className="main">
+        <Canvas1></Canvas1>
+        <Canvas2></Canvas2>
+        <Canvas3></Canvas3>
+        <Canvas4></Canvas4>
+      </main>
+    </body>
   );
 }
 
